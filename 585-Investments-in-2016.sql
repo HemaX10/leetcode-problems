e1f430fc0987af80b536cd87e55d583124198551
ابراهيم ايمن عبدالعزيز زكي <124893046+HemaX10@users.noreply.github.com>
2024-10-12 00:01:18 +0300

    [585] [Time Beats: 78.48%] - LeetPush

--- 585-Investments-in-2016.sql
+++ 585-Investments-in-2016.sql
@@ -1,11 +1,15 @@
 # Write your MySQL query statement below
-select round(sum(ins1.tiv_2016),2) as tiv_2016
-from insurance ins1 join insurance ins2
-on ins1.pid = ins2.pid
-where ins1.tiv_2015 in (
-    select tiv_2015 from insurance where pid != ins1.pid
-) and ins1.pid in (
-    select pid from insurance group by lat , lon having count(pid) = 1
-)
-
-
+select round(sum(tiv_2016) , 2)as tiv_2016
+from insurance 
+where pid  in (
+select pid from (
+select pid , dense_rank() over(order by lat , lon ) as dr
+from insurance ) as newTable
+group by dr 
+having count(dr) = 1) 
+and pid not in (
+    select pid from (    
+    select pid , count(tiv_2015) as countt
+    from insurance
+    group by tiv_2015 ) test1 
+    where countt = 1)
